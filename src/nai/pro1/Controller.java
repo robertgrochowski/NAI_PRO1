e72e627308a7510f84b295e380caef3ab46815f3
robertgrochowski <robert.grochowski98@gmail.com>
2019-03-28 23:26:01 +0100

    Added User Interface

---
+++ src/nai/pro1/Controller.java
@@ -0,0 +1,42 @@
+package nai.pro1;
+
+import javafx.fxml.FXML;
+import javafx.scene.Node;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.GridPane;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.Paint;
+import javafx.scene.shape.Rectangle;
+
+public class Controller {
+
+    @FXML
+    private void mouseEntered(MouseEvent e) {
+        Node source = (Node)e.getSource() ;
+        Integer colIndex = GridPane.getColumnIndex(source);
+        Integer rowIndex = GridPane.getRowIndex(source);
+        System.out.printf("Mouse entered cell [%d, %d]%n", colIndex, rowIndex);
+
+        if(source instanceof Rectangle)
+            ((Rectangle) source).setFill(Color.ROYALBLUE);
+    }
+
+    @FXML
+    private void mouseClicked(MouseEvent e) {
+        Node source = (Node)e.getSource() ;
+        Integer colIndex = GridPane.getColumnIndex(source);
+        Integer rowIndex = GridPane.getRowIndex(source);
+        System.out.printf("Mouse clicked cell [%d, %d]%n", colIndex, rowIndex);
+    }
+
+    @FXML
+    private void mouseExit(MouseEvent e) {
+        Node source = (Node)e.getSource() ;
+        Integer colIndex = GridPane.getColumnIndex(source);
+        Integer rowIndex = GridPane.getRowIndex(source);
+        System.out.printf("Mouse exit cell [%d, %d]%n", colIndex, rowIndex);
+
+        if(source instanceof Rectangle)
+            ((Rectangle) source).setFill(Color.TRANSPARENT);
+    }
+}
